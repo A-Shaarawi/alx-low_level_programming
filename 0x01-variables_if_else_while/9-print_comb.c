3850f6305796beb664a2cdad0766f605e13a11f9
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-14 10:58:18 -0700

    C2

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	int y;
+
+	int z;
+
+x = 48;
+y = 44;
+z = 32;
+while (x < 58)
+{
+putchar(x);
+putchar(y);
+putchar(z);
+x++;
+}
+putchar('\n');
+return (0);
+}
