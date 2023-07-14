ff2354568c7a0e39fe757b7e15382c718cf11fd8
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-14 10:46:38 -0700

    C2

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	x = 48;
+while (x < 58)
+{
+putchar(x);
+x++;
+}
+putchar('\n');
+return (0);
+}a
