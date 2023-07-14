ff50866b7013aa45f976e685104004d93d167269
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-14 10:34:05 -0700

    C2

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	x = 'a';
+	while (x <= 'z')
+	{   
+	if (x == 'e' || x == 'q')
+	{
+	x++;
+	}   
+	putchar(x);
+	x++;
+	};
+	putchar('\n');
+	return (0);
+}
