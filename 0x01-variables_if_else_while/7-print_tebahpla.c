c4196225c0fef4ad844bf8d89aa46b047f59cc69
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-14 10:49:11 -0700

    C2

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	x = 'z';
+while (x >= 'a')
+{
+putchar(x);
+x--;
+}
+putchar('\n');
+return (0);
+}
