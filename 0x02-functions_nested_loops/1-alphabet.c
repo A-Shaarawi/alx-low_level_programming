687d70003a05df3561d97094debefc32e3c3dc2f
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-18 03:33:42 -0700

    C3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_alphabet
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
