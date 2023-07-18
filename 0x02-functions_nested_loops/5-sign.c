092cd98b03b23508bf381a7953cd873f92106c5f
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-18 14:10:04 -0700

    C3

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar('+');
+putchar(43);
 return (1);
 }
 if (n < 0)
 {
-printf("-");
+putchar(45)
 return (-1);
 }
 else
 {
-printf("0");
+putchar(48)
 return (0);
 }
 return (n);
