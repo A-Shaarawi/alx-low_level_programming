aca3e21caf957118bdd8d478c08cc92b1c644b20
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-18 04:36:07 -0700

    C3

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * print_sign - numbers
- * @c: only paarameter
+ * @n: only paarameter
  * Return: Always 0 (Success)
  */
 
-int print_sign(int c)
+int print_sign(int n)
 {
 if (n > 0)
 {
