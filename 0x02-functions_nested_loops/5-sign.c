47aed642b8645b0e927dfec866a9a5a15a274b98
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-18 04:34:16 -0700

    C3

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * print_sign - numbers
+ * @c: only paarameter
+ * Return: Always 0 (Success)
+ */
+
+int print_sign(int c);
+{
+if (n > 0)
+{
+printf("+")
+return (1);
+}
+if (n < 0)
+{
+printf("-")
+return (-1);
+}
+else
+{
+printf("0")
+return (0);
+}
+return (c);
