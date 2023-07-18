43f0c828c2cefde7ac268e09dc8217e0268cbf84
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-18 03:49:14 -0700

    C3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,6 +6,23 @@
  * print_alphabet() - prints
  * Return: Always 0 (Success)
  */
+/**
+ * print_alphabet - Brief description of function.
+ * void: Describe the first argument.
+ *
+ * A longer description, with more discussion of the function function_name()
+ * that might be useful to those using or modifying it. Begins with an
+ * empty comment line, and may include additional embedded empty
+ * comment lines.
+ *
+ * The longer description may have multiple paragraphs.
+ *
+ * Context: Describes whether the function can sleep, what locks it takes,
+ *          releases, or expects to be held. It can extend over multiple
+ *          lines.
+ * Return: Describe the return value of function_name.
+ *
+ */
 
 void print_alphabet(void)
 {
