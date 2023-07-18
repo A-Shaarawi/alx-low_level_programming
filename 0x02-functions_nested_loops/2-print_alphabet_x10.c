a747d68fdaa5146663da6c0792bba6a14096c7fb
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-18 04:02:45 -0700

    C3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,17 +3,18 @@
  * print_alphabet_x10 - alphabet x10
  * Return: Always 0 (Success)
  */
-
-void print_alphabet_x10()
+void print_alphabet_x10(void)
 {
 int counter;
 
 char x;
 
 counter = 1;
+
 while (counter <= 10)
 {  
 x = 'a';
+
 while (x <= 'z')
 {
 putchar(x);
