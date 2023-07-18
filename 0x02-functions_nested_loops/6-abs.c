fe315578a702264c119ef5807f193942cb7e12ff
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-18 14:27:42 -0700

    C3

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.c"
+#include <stdio.h>
+/**
+ * int _abs - numbers
+ * @c: only paarameter
+ * Return: Always 0 (Success)
+ */
+int _abs(int c)
+{
+if (c < 0)
+{
+return (-1 * c);
+}
+else
+{
+return (c);
+}
+}
