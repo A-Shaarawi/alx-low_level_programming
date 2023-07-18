bd5d574d478373dfcd94e8e1eef56ed1e9414573
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-18 04:14:42 -0700

    C3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * _islower - alphabet x10
+ * @c: only parameter
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (c);
+}
