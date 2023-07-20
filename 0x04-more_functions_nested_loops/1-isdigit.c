1b84e14fb62ce308c7cbe2ac0b07b0d92c429f06
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-20 06:48:21 -0700

    C5

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - returns the largest of 3 numbers
+ * @c: para
+ * Return: largest number
+ */
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
