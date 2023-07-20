7ee9628d617f1f48477b652241e86f72644a6c3d
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-20 06:55:47 -0700

    C5

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - returns the largest of 3 numbers
+ * Return: largest number
+ */
+
+void print_numbers(void)
+{
+for (int i = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
