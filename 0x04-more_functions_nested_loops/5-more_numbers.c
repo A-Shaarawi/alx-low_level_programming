0a393ddaeee859b751cf608b543257b6934ae277
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-20 07:28:17 -0700

    C5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,24 +1,22 @@
 #include "main.h"
+
 /**
  * more_numbers - print more numbers
  */
 
 void more_numbers(void)
 {
-int i = 1;
+	int i, j;
 
-while (i <= 10)
-{
-int j = 0;
-while (j <= 14)
-{
-if (j >= 10)
-putchar('1');
-putchar(j % 10 + '0');
-j++;
-putchar('\n');
-i++;
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar (j % 10 + '0');
+		}
+		_putchar('\n');
+	}
 }
-return (0);
-}
-}
+
