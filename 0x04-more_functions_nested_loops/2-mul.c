42f589b19ffaffa7bbfa0d7c7c62cd4353e056dc
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-20 06:52:06 -0700

    C5

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - returns the largest of 3 numbers
+ * @a: para
+ * @b: para
+ * Return: largest number
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
