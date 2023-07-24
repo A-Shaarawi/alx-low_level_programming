c60a7fa037d6ed1108f36cf0681a0783c00ef4c8
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-24 04:57:54 -0700

    C6

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - funtion
+ * @s: parameter
+ * Return: number
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != NULL)
+	{
+		cout++;
+		*s++;
+	}
+	return (count);
+}
