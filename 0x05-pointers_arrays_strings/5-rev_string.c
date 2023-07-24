7e0cf74106ee5f57d2d9518bf37032c3cdd5c6eb
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-24 05:49:49 -0700

    C6

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,23 +9,22 @@
 
 void rev_string(char *s)
 {
-	char temp = s[0];
+        char temp = s[0];
 
-	int count = 0;
+        int count = 0;
 
-	int i;
+        int i;
 
-	i = 0;
-	while (s[count] != '\0')
-	{
-			count++;
-	}
-			while (i < count)
-			{
-				count--;
-				temp = s[i];
-				s[i] = s[count];
-				s[count] = temp;
-				i++;
-			}
+while (s[count] != '\0')
+        {
+                        count++;
+        }
+                        for (i=0; i < count; i++)
+                        {
+                                count--;
+                                temp = s[i];
+                                s[i] = s[count];
+                                s[count] = temp;
+                        }
 }
+
---
+++ 0x05-pointers_arrays_strings/5-rev_string_temp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - funtion
+ * @s: parameter
+ * Return: number
+ */
+
+void rev_string(char *s)
+{
+	char temp = s[0];
+
+	int count = 0;
+
+	int i;
+
+	i = 0;
+while (s[count] != '\0')
+	{
+			count++;
+	}
+			while (i < count)
+			{
+				count--;
+				temp = s[i];
+				s[i] = s[count];
+				s[count] = temp;
+				i++;
+			}
+}
