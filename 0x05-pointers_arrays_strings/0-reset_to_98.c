8f79e47e2c07f87165bc98b96619ae93c84c5f30
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-24 04:21:26 -0700

    C6

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - funtion
+ * @n: parameter
+ * Return: number
+ */
+
+void reset_to_98(int *n)
+{
+	int n;
+
+	int *p;
+
+	p = &n;
+	*p = 98;
+}
