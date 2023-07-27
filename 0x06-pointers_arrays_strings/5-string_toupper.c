cdf2c5d9f87dbdd208f492df0169cd6da546f2ca
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-27 07:02:46 -0700

    C7

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - funtion
+ * @n: parameter
+ * Return: number
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] > 'a' && n[i] < 'z')
+		{
+			n[i] = n[i] - 32;
+		}
+		i++;
+	}
+	return (n);
+}
