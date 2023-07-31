0930b765205965ed1e09e74535ff1a3fbab99e77
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-07-31 14:43:27 -0700

    C8

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -23,8 +23,10 @@ unsigned int _strspn(char *s, char *accept)
 				n++;
 				break;
 			}
-			else if (accept[i+1] == '\0')
-					return (n);
+			else if (accept[i + 1] == '\0')
+			{
+				return (n);
+			}
 		}
 		s++;
 	}
