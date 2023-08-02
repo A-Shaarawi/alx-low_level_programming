1130f63cdb1bcfd76cacaca531553f1275508792
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-08-02 07:44:24 -0700

    C9

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - function
+ * @x: parameter
+ * @y: parameter
+ * Return: number
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
