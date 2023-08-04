668e0950dd2a5886ffcd30bb2936e85568b70941
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-08-04 06:26:24 -0700

    M10

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: parameter
+ * @argv: parameter
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s", argv[i]);
+		printf("\n");
+	}
+	return (0);
+}
