96fe7805130ab07b087a95aff704362028e3833b
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-08-07 05:45:30 -0700

    malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - function
+ * @s1: parameter
+ * @s2: parameter
+ * Return: number
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *connect;
+
+	int i1;
+
+	int i2;
+
+	i1 = i2 = 0;
+	while (s1[i1] != '\0')
+		i1++;
+	while (s2[i2] != '\0')
+		i2++;
+	connect = malloc(sizeof(char) * (i1 + i2 + 1));
+	if (connect == NULL)
+		return (NULL);
+	i1 = i2 = 0;
+	while (s1[i1] != '\0')
+	{
+		connect[i1] = s1[i1];
+		i1++;
+	}
+	while (s2[i2] != '\0')
+	{
+		connect[i1] = s2[i2];
+		i2++;
+		i1++;
+	}
+	connect[i1] = '\0';
+	return (connect);
+}
