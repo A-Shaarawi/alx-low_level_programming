c2686deac7b1410b859e7f83bfd8bdf15ce7910f
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-08-07 06:27:25 -0700

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,14 +9,37 @@
  * Return: number
  */
 
-int **alloc_grid(int width, int height)
 {
-	int i;
+	int **mee;
+	int x, y;
 
-	for (i = 0; i < height; i++)
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	mee = malloc(sizeof(int *) * height);
+
+	if (mee == NULL)
+		return (NULL);
+
+	for (x = 0; x < height; x++)
 	{
-		free(width[i]);
-	}
-	free(width);
-}
+		mee[x] = malloc(sizeof(int) * width);
 
+		if (mee[x] == NULL)
+		{
+			for (; x >= 0; x--)
+				free(mee[x]);
+
+			free(mee);
+			return (NULL);
+		}
+	}
+
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+			mee[x][y] = 0;
+	}
+
+	return (mee);
+}
