70fd74301d9a1abf046bbe00be109c2bc57304fd
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-08-10 06:47:22 -0700

    Malloc 2

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,18 +14,16 @@ int *array_range(int min, int max)
 	int  *ptr;
 	int i, j;
 
-	if(min > max)
+	if (min > max)
 	{
 		return (NULL);
 	}
 	j = max - min + 1;
 	ptr  = malloc(sizeof(int) * j);
-	if(ptr == NULL)
+	if (ptr == NULL)
 		return (NULL);
-	for(i = 0; min <=max; i++)
-		{
-			ptr[i] = min;
-			min++;
-		}
+
+	for (i = 0; min <= max; i++)
+		ptr[i] = min++;
 	return (ptr);
 }
