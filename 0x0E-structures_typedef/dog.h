1fd1342427cf6096cfc81b0f545a85e68bb31e16
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-08-14 07:57:05 -0700

    Struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dogg
+#define dogg
+#include <stdio.h>
+/**
+ * struct dog - struct
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ * Description: 'dog desc'
+ */
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+}
+#endif
