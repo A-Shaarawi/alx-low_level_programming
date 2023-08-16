53c3543e58ff72492f033657541578d6733e028d
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-08-16 06:09:29 -0700

    Function Pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function
+ * @array: para
+ * @size: para
+ * @action: para
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
