24462bcbaa05ea51d57afae21cda35380e8b9842
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-08-17 08:09:36 -0700

    C11

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,26 +18,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
-	if (separator == NULL)
-	{
 		for (i = 0; i < n; i++)
 		{
-			x = va_arg(args, int);
+			x = va_arg(args, unsigned int);
 			printf("%d", x);
-		}
-	}
-	if (separator != NULL)
-	{
-		for (i = 0; i < n; i++)
-		{
-		x = va_arg(args, int);
-		if (i < n - 1)
-			printf("%d%s", x, separator);
-		else if (i == n - 1)
-			printf("%d", x);
+			if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
 		}
 		printf("\n");
-	}
 	va_end(args);
 }
 
