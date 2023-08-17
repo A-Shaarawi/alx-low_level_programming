71a5175bc00bc9350ff829c630bfc921c25ca8f4
a-shaarawi <abdelrahmangamal305@gmail.com>
2023-08-17 07:48:32 -0700

    C11

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - function
+ * @separator: parameter
+ * @n: parameter
+ * Return: number
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...);
+{
+	int x;
+
+	unsigned int i;
+
+	va_list args;
+
+	if (separator == NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			x = va_arg(args, unsigned int);
+			printf("%d", x);
+		}
+	}
+	if (separator == NULL)
+	{
+		x = va_arg(args, unsigned int);
+		if (i < n - 1)
+			printf("%d%s", x, separator);
+		else if (i == n - 1)
+			printf("%d", x);
+	}
+	va_end(args);
+}
+
