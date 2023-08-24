d80327636f2498520fb45cd9eb5c942362d30899
A-Shaarawi <abdelrahmangamal305@gmail.com>
2023-08-24 07:39:37 -0700

    Linked-List

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,6 @@
+#include <stdlib.h>
 #include "lists.h"
-
+#include <string.h>
 /**
  * list_len - function
  * @h: parameter
@@ -13,7 +14,7 @@ size_t list_len(const list_t *h)
 	while (h)
 	{
 		h = h->next;
-		i++
+		i++;
 	}
 	return (i);
 }
