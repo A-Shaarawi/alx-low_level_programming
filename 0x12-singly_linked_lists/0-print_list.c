70850311f61c783decd6ca7b593d716358974c0b
A-Shaarawi <abdelrahmangamal305@gmail.com>
2023-08-24 07:06:00 -0700

    Linked-List

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,13 +10,13 @@ size_t print_list(const list_t *h)
 {
 	size_t i;
 
-for (i = 0; h; i++)
-{
-	if (h->str == NULL)
-		printf("[0] (nil)\n");
-	else
-		printf("[%u] %s\n", h->len, h->str);
-	h = h->next;
-}
+	for (i = 0; h; i++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+	}
 	return (i);
 }
