31b93aedbe720f1a2a24f5e805ffe0297e97992f
A-Shaarawi <abdelrahmangamal305@gmail.com>
2023-08-29 07:05:53 -0700

    Linked-List-2

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - fucntion
+ * @h: para
+ * Return: number
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+	printf("%d\n",h->n);
+	i++;
+	h = h->next;
+	}
+	return (i);
+}
