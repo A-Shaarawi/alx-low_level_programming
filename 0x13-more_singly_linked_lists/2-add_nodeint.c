c8e32738da4c0b55bc11590706a0d387704396d7
A-Shaarawi <abdelrahmangamal305@gmail.com>
2023-08-29 07:35:01 -0700

    Linked-List-2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,9 +10,9 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-listint_s *new;
+struct listint_s *new;
 
-new= malloc(sizeof(listint_s));
+new = malloc(sizeof(struct listint_s));
 new->n = n;
 new->next = *head;
 *head = new;
