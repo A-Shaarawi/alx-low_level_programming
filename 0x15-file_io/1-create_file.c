266b9a8248357ccf0f2a98ec2b151c3315ddf14e
A-Shaarawi <abdelrahmangamal305@gmail.com>
2023-09-04 09:52:48 -0700

    File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,15 +12,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content != NULL)
-	{
-		for (i = 0; text_content[i];)
-			i++;
-	}
+
+	while (text_content[i])
+		i++;
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 	w = write(fd, text_content, i);
 	if (fd == -1 || w == -1)
 		return (-1);
+
 	close(fd);
 	return (1);
 }
