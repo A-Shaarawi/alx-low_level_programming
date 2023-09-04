5dc7bc6ccd1437b6a56efd615b21b0be7ebdadf7
A-Shaarawi <abdelrahmangamal305@gmail.com>
2023-09-04 10:01:12 -0700

    File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,6 @@ int append_text_to_file(const char *filename, char *text_content)
 
         fd = open(filename, O_WRONLY | O_APPEND);
         w = write(fd, text_content, i);
-
         if (fd == -1 || w == -1)
                 return (-1);
 
